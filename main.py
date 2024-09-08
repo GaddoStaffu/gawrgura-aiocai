b60e814d59bd03f33a4dfc46d3deedf39f859a11
GaddoStaffu <jomarilagumen02@gmail.com>
2024-09-08 18:34:48 +0800

    commit

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ print("Waking up Gawr Gura....")
 
 # Run the asynchronous function using asyncio
 asyncio.run(gawrgura_aiocai.talktoai())
-
-# Print the final response from Gawr Gura
-print(gawrgura_aiocai.gawr_says)
-
-print()
