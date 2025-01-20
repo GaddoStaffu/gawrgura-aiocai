0f16c72014cba5d1bac69709ad0d4f3e4671c5ef
Jomari <162972412+GaddoStaffu@users.noreply.github.com>
2025-01-20 16:55:58 +0800

    Revert "Added voice interaction"
    This reverts commit 975e99d0040284d16b60a792523f16590e50451d.

--- gawrgura_aiocai.py
+++ gawrgura_aiocai.py
@@ -1,9 +1,9 @@
 from characterai import aiocai
 import pyttsx3
-import speech_recognition as sr
+
 # Initialize the text-to-speech engine
 engine = pyttsx3.init()
-r = sr.Recognizer()
+
 # Set up the voice properties
 def setup_voice():
     """Setup voice rate, volume, and voice type."""
@@ -31,49 +31,20 @@ async def talktoai():
             print(f'{answer.name}: {answer.text}')
             
             while True:
-                print("Listening.....")
-                userreply = talk()
-                print(f'You Said: {userreply}')
-                message = await chat.send_message(char, new.chat_id, userreply) # message text to talk to character 
+                text = input('YOU: ')
+                if text.lower() in ["cmd_exit"]:
+                    print("Ending conversation...")
+                    break
+                message = await chat.send_message(char, new.chat_id, text)
                 print(f'{message.name}: {message.text}')
-
                 
                 # Update the message and speak it out
                 gawr_says = message.text
-                engine.say(gawr_says) # calling a tts library to say the message
-                engine.runAndWait() # wait for the tts to finish talking to continue
+                engine.say(gawr_says)
+                engine.runAndWait()
 
         engine.stop()  # Ensure the engine stops after the conversation
 
     except Exception as e:
         print(f"An error occurred: {e}")
         engine.stop()  # Stop the engine if an error occurs
-
-
-
-def talk()->str:
-    while(1):
-
-        # Exception handling to handle
-        # exceptions at the runtime
-        try:
-
-            # use the microphone as source for input.
-            with sr.Microphone() as source2:
-                # wait for a second to let the recognizer
-                # adjust the energy threshold based on
-                # the surrounding noise level
-                r.adjust_for_ambient_noise(source2, duration=0.2)
-                #listens for the user's input
-                audio2 = r.listen(source2)
-                # Using google to recognize audio
-                MyText = r.recognize_google(audio2)
-                MyText = MyText.lower()
-                
-                return MyText
-
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
-
-        except sr.UnknownValueError:
-            print("Sorry, can you repeat that?")
